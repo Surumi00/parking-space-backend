d1fbfc8e4d61d1f151cb7f59adf02b1faf1f9904
ShaunPeter04 <shaunpeter04@gmail.com>
2026-07-15 10:38:49 +0530

    Mongodb connected

---
+++ Vehicle.js
@@ -0,0 +1,37 @@
+
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+
+mongoose.connect("mongodb+srv://shaun:ares2008@cluster0.dsyjzb7.mongodb.net/vehicle-db?appName=Cluster0").then(
+
+    () => {
+        console.log("mongodb connected")
+    }
+
+).catch((error) => {
+    console.log(error)
+})
+
+const Vehicle = mongoose.model("vehicles", new mongoose.Schema(
+    {
+        vehicleId: String,
+        ownerName: String,
+        vehicleNumber: String,
+        vehicleModel: String,
+        vehicleBrand: String,
+        vehicleType: String,
+        vehicleColor: String,
+        registrationDate: Date,
+        ownerContactNumber: String,
+        ownerEmail: String,
+        parkingPassType: String,
+        address: String
+    }
+))
